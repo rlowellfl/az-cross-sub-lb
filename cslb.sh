e2d3b7a042d788b4d16572cbb5b7483a5b081048
Ryan Lowell <ryan.lowell@outlook.com>
2025-03-19 01:48:22 -0400

    Replaced values with environment variables

--- cslb.sh
+++ cslb.sh
@@ -1,12 +1,18 @@
+export TENANT_ID="<your tenant ID>"
+export SUB_A_NAME="<name of subscription A>"
+export SUB_A_ID="<subscription ID of subscription A>"
+export SUB_B_NAME="<name of subscription B>"
+export SUB_B_ID="<subscription ID of subscription B>"
+
 # Sign in to Azure CLI
-Az login --tenant <your tenant ID here> --use-device-code
+Az login --tenant $TENANT_ID --use-device-code
 
 ###########
 ### Initial setup, subscription B
 ###########
 
-# Change subscription to Azure Subscription B (<subscription B ID>) where the backend virtual network and load balancer pool will reside
-az account set --name '<subscription B name>'
+# Change subscription to Azure Subscription B where the backend virtual network and load balancer pool will reside
+az account set --name $SUB_B_NAME
 
 # Create a resource group in Azure Subscription B 
 az group create --name 'rg-cslb-back' --location centralus
@@ -18,8 +24,8 @@ az network vnet create -g rg-cslb-back -n vnet-back --address-prefix 10.2.0.0/16
 ### Initial setup, subscription A
 ###########
 
-# Change subscription to Azure Subscription A (<subscription A ID>) where the load balancer and front end will reside
-az account set --name '<subscription A name>'
+# Change subscription to Azure Subscription A where the load balancer and front end will reside
+az account set --name $SUB_A_NAME
 
 # Create a resource group in Azure Subscription A 
 az group create --name 'rg-cslb-front' --location centralus
@@ -32,7 +38,7 @@ az group create --name 'rg-cslb-front' --location centralus
 az network lb create --resource-group rg-cslb-front --name cslb --sku Standard --frontend-ip-name ip-front --tags 'IsRemoteFrontend=true'
 
 # Create a back end pool in the vnet of Subscription B
-az network lb address-pool create --address-pool-name pool-back --lb-name cslb --resource-group rg-cslb-front --vnet '/subscriptions/<subscription B ID>/resourceGroups/rg-cslb-back/providers/Microsoft.Network/virtualNetworks/vnet-back' --sync-mode Automatic
+az network lb address-pool create --address-pool-name pool-back --lb-name cslb --resource-group rg-cslb-front --vnet '/subscriptions/'$SUB_B_ID'/resourceGroups/rg-cslb-back/providers/Microsoft.Network/virtualNetworks/vnet-back' --sync-mode Automatic
 
 # Create a health probe
 az network lb probe create --resource-group rg-cslb-front --lb-name cslb --name myHealthProbe --protocol tcp --port 80
@@ -41,15 +47,15 @@ az network lb probe create --resource-group rg-cslb-front --lb-name cslb --name 
 az network lb rule create --resource-group rg-cslb-front --lb-name cslb --name myHTTPRule --protocol tcp --frontend-port 80 --backend-port 80 --frontend-ip-name ip-front --backend-pool-name pool-back --probe-name myHealthProbe --disable-outbound-snat true --idle-timeout 15 --enable-tcp-reset true
 
 # Set the subscription context to Azure Subscription B
-az account set --name '<subscription B name>'
+az account set --name $SUB_B_NAME
 
-# Create and attach a network interface card to the load balancer
-az network nic create --name nic-back --resource-group rg-cslb-back --vnet vnet-back --subnet back --lb-address-pool '/subscriptions/<subscription A ID>/resourceGroups/rg-cslb-front/providers/Microsoft.Network/loadBalancers/cslb/backendAddressPools/pool-back'
+# Attach the network interface card to the load balancer
+az network nic create --name nic-back --resource-group rg-cslb-back --vnet vnet-back --subnet back --lb-address-pool '/subscriptions/'$SUB_A_ID'/resourceGroups/rg-cslb-front/providers/Microsoft.Network/loadBalancers/cslb/backendAddressPools/pool-back'
 
 ###########
 ### Clean up
 ###########
 
 az group delete --name rg-cslb-back --no-wait --yes
-az account set --name '<subscription B name>'
+az account set --name $SUB_A_NAME
 az group delete --name rg-cslb-front --no-wait --yes
